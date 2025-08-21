e76402868713b054e8d83f027c3920550e902234
Frvncois <hello@frvncois.com>
2025-08-21 12:02:55 -0400

    Fix Middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,9 +4,12 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5174'],
+      origin: [
+        'http://localhost:5174',
+        'https://traq-front.vercel.app',
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       credentials: true,
     },
   },
